f19eecb61fa1c30dd42c18ea66f19af8f7852eb8
mpheng666 <mpheng96@gmail.com>
2022-10-18 00:04:08 +0800

    Fix grid with rainbow

--- src/main.cpp
+++ src/main.cpp
@@ -2,65 +2,30 @@
 
 int main() {
     srand(time(0));
+    int height = 600;
+    int width = 600;
+    int num_row = 10;
+    int num_col = 10;
+    double scale_for_line = 0.9;
+    double scale_for_offset = (1 - scale_for_line) / 2;
 
-    sf::RenderWindow window(sf::VideoMode(400, 400), "Mutli-Agent-Simulator",
+    sf::RenderWindow window(sf::VideoMode(width, height),
+                            "Mutli-Agent-Simulator",
                             sf::Style::Titlebar | sf::Style::Close);
     window.setFramerateLimit(60);
+    int grid_w = width / num_col;
+    int grid_h = height / num_row;
 
-    int w = 32;
-    int grid[12][12];
-    int sgrid[12][12]; // for showing
-
-    sf::Texture t;
-    sf::Texture black_square_texture;
-    black_square_texture.loadFromFile("../images/black_with_white_corners.png");
-    // t.loadFromFile("../images/poly_light_blue.png");
-    t.loadFromFile("../images/tiles.jpg");
-    sf::Sprite s(t);
-    sf::Sprite black_square_sprite(black_square_texture);
-
-    sf::Vector2u black_square_size = black_square_texture.getSize();
-    // black_square_texture.
-
-    for (int i = 1; i <= 10; i++) {
-        for (int j = 1; j <= 10; j++) {
-            sgrid[i][j] = 10;
-            if (rand() % 5 == 0)
-                grid[i][j] = 9;
-            else
-                grid[i][j] = 0;
-        }
-    }
-
-    for (int i = 1; i <= 10; i++) {
-        for (int j = 1; j <= 10; j++) {
-            int n = 0;
-            if (grid[i][j] == 9)
-                continue;
-            if (grid[i + 1][j] == 9)
-                n++;
-            if (grid[i][j + 1] == 9)
-                n++;
-            if (grid[i - 1][j] == 9)
-                n++;
-            if (grid[i][j - 1] == 9)
-                n++;
-            if (grid[i + 1][j + 1] == 9)
-                n++;
-            if (grid[i - 1][j - 1] == 9)
-                n++;
-            if (grid[i - 1][j + 1] == 9)
-                n++;
-            if (grid[i + 1][j - 1] == 9)
-                n++;
-            grid[i][j] = n;
-        }
-    }
+    sf::Texture blue_texture;
+    // blue_texture.loadFromFile("../images/poly_light_blue.png");
+    blue_texture.loadFromFile("../images/rainbow_square.png");
+    sf::Sprite sprite_blue(blue_texture);
+    // sprite_blue.scale(0.6, 0.6);
 
     while (window.isOpen()) {
         sf::Vector2i pos = sf::Mouse::getPosition(window);
-        int x = pos.x / w;
-        int y = pos.y / w;
+        // int x = pos.x / w;
+        // int y = pos.y / w;
 
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -68,34 +33,32 @@ int main() {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
-            if (event.type == sf::Event::MouseButtonPressed)
-            {
+            if (event.type == sf::Event::MouseButtonPressed) {
                 // if (event.key.code == sf::Mouse::Left)
                 //     sgrid[x][y] = grid[x][y];
                 // else if (event.key.code == sf::Mouse::Right)
                 //     sgrid[x][y] = 11;
             }
         }
-        
+
         window.clear(sf::Color::Black);
 
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 0; i <= num_col; i++) {
+            for (int j = 0; j <= num_row; j++) {
+                sprite_blue.setTextureRect(sf::IntRect(
+                        i * grid_w, j * grid_h, grid_w * scale_for_line,
+                        grid_h * scale_for_line));
 
-            for (int j = 1; j <= 10; j++) {
-                if (sgrid[x][y] == 9)
-                {
-                    sgrid[i][j] = grid[i][j];
-                }
-                s.setTextureRect(sf::IntRect(sgrid[i][j] * w, 0, w, w));
-                black_square_sprite.setTextureRect(sf::IntRect(sgrid[i][j] * w, 0, w, w));
-                s.setPosition(i * w, j * w);
-                black_square_sprite.setTextureRect(
-                        sf::IntRect(sgrid[3][5] * w, 0, w, w));
+                sprite_blue.setPosition(i * grid_w + scale_for_offset * grid_w,
+                                        j * grid_h + scale_for_offset * grid_h);
 
-                window.draw(s);
-                window.draw(black_square_sprite);
+                window.draw(sprite_blue);
             }
         }
+        // sprite_blue.setTextureRect(sf::IntRect(0, 0, grid_w, grid_h));
+        // sprite_blue.setPosition(10, 0);
+        // window.draw(sprite_blue);
+
         window.display();
     }
 
