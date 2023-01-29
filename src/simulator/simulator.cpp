ef6a13b748bf67554bc76151f3152b8f92fb8dde
mpheng666 <mpheng96@gmail.com>
2023-01-29 15:47:28 +0800

    Update main

--- src/simulator/simulator.cpp
+++ src/simulator/simulator.cpp
@@ -11,7 +11,7 @@ Simulator::Simulator(const int window_width,
     : rwindow_(sf::VideoMode(window_width, window_height),
                window_name,
                sf::Style::Titlebar | sf::Style::Close)
-    , map_(col, row)
+    , map_(300, 300, 10, 10, rwindow_)
     , width_step_(window_width / col)
     , height_step_(window_height / row)
 {
@@ -19,13 +19,11 @@ Simulator::Simulator(const int window_width,
 
 void Simulator::run()
 {
-    initialize_map();
     initialize_objects(agent_);
     rwindow_.setFramerateLimit(60);
     // mas_path_finding_.start(map_, 10, 10, 50, 60, grid_w_, grid_h_);
 
     while (rwindow_.isOpen()) {
-
         sf::Event event;
         while (rwindow_.pollEvent(event)) {
             switch (event.type) {
@@ -58,36 +56,10 @@ void Simulator::run()
             }
         }
 
-        rwindow_.clear(sf::Color::Black);
-
-        // Rendering
-        for (auto i = 0; i < map_.getColumnSize(); ++i) {
-            for (auto j = 0; j < map_.getRowSize(); ++j) {
-                float position_x = i * grid_w_;
-                float position_y = j * grid_h_;
-                map_.grid_map.at(i).at(j).setPosition(position_x, position_y);
-                rwindow_.draw(map_.grid_map.at(i).at(j));
-            }
-        }
-
+        map_.renderMap();
         rwindow_.draw(agent_);
-
         rwindow_.display();
-    }
-}
-
-void Simulator::initialize_map()
-{
-    grid_w_ = rwindow_.getSize().x / map_.getColumnSize();
-    grid_h_ = rwindow_.getSize().y / map_.getRowSize();
-
-    for (auto i = 0; i < map_.getColumnSize(); ++i) {
-        for (auto j = 0; j < map_.getRowSize(); ++j) {
-            map_.grid_map.at(i).at(j).setSize(sf::Vector2f(grid_w_, grid_h_));
-            map_.grid_map.at(i).at(j).setFillColor(sf::Color::White);
-            map_.grid_map.at(i).at(j).setOutlineColor(sf::Color::Black);
-            map_.grid_map.at(i).at(j).setOutlineThickness(1.0f);
-        }
+        rwindow_.clear();
     }
 }
 
