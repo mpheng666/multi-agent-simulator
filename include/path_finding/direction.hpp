111681d92539fa8a7729443bf1f930aed61ece86
mpheng666 <mpheng96@gmail.com>
2023-02-15 22:34:37 +0800

    Add path finding direction

---
+++ include/path_finding/direction.hpp
@@ -0,0 +1,13 @@
+#ifndef MAS_PATH_FINDING_DIRECTION_HPP_
+#define MAS_PATH_FINDING_DIRECTION_HPP_
+
+namespace mas
+{
+    enum class PathFinderDirection
+    {
+        CARDINAL,
+        ORDINAL
+    };
+}
+
+#endif
